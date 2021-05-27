07e4a54e1aaaf22b9e01ce559057e3c6835f6b06
alfianfadhil <alvian.wadada@gmail.com>
2021-05-27 16:21:48 +0700

    set button disable

--- src/components/Editprofile.js
+++ src/components/Editprofile.js
@@ -151,7 +151,7 @@ function Editprofile(props) {
                 </div>
                 <div className=" align-self-end button-edit mt-2">
                   <button
-                    disabled
+                    disabled={disable}
                     onClick={submitProfile}
                     className="btn btn-sm btn-edit-profile"
                   >
